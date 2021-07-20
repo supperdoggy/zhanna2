66c6dcdca7420831629c588120228ae6e2c9ba01
Maksym <supperspggy229@gmail.com>
2021-07-20 22:26:39 +0300

    bugfix

--- bot/internal/communication/communication.go
+++ bot/internal/communication/communication.go
@@ -57,6 +57,7 @@ func MakeUserHttpReq(method string, req, resp interface{}) (err error) {
 func UpdateUser(usermsg, botmsg *telebot.Message) {
 	if usermsg == nil || botmsg == nil {
 		fmt.Println("got nil botmsg or usermsg")
+		return
 	}
 	var req structs.User = structs.User{
 		Telebot: *usermsg.Sender,
