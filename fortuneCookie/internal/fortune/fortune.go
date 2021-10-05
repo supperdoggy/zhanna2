a8ad659997176d5aa48900a9b0a7ab7c391de71d
Maksym <supperspggy229@gmail.com>
2021-10-05 22:30:31 +0300

    logging cookies

--- fortuneCookie/internal/fortune/fortune.go
+++ fortuneCookie/internal/fortune/fortune.go
@@ -4,10 +4,12 @@ import (
 	"github.com/supperdoggy/superSecretDevelopement/fortuneCookie/internal/db"
 	"github.com/supperdoggy/superSecretDevelopement/structs"
 	fortunedata "github.com/supperdoggy/superSecretDevelopement/structs/request/fortune"
+	"go.uber.org/zap"
 )
 
 type Service struct {
 	DB db.DbStruct
+	Logger *zap.Logger
 }
 
 func (s Service) GetRandomFortuneCookie() (resp fortunedata.GetRandomFortuneCookieResp) {
@@ -15,6 +17,7 @@ func (s Service) GetRandomFortuneCookie() (resp fortunedata.GetRandomFortuneCook
 
 	cookie, err := s.DB.GetRandomFortune()
 	if err != nil {
+		s.Logger.Error("error GetRandomFortune", zap.Error(err), zap.Any("resp", resp))
 		resp.Err = err.Error()
 		return
 	}
