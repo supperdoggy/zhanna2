395a37d3ded49c418d0b158f056daa55b9865849
supperdoggy <supperspggy229@gmail.com>
2021-10-21 13:14:18 +0300

    moved ctx to avoid creating useless variable

--- bot/internal/config/config.go
+++ bot/internal/config/config.go
@@ -16,9 +16,9 @@ type config struct {
 var c config
 
 func GetConfig(logger *zap.Logger) *config {
-	ctx := context.Background()
 	once := sync.Once{}
 	once.Do(func() {
+		ctx := context.Background()
 		if err := envconfig.Process(ctx, &c); err != nil {
 			logger.Fatal("error processing config", zap.Error(err))
 		}
